551473f49ef8f6db20940a0623ad9380dbcd6e99
Oleksandr Bryksin <oleksandr.bryksin@hys-enterprise.com>
2025-07-16 21:09:52 +0300

    eslint + prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,93 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next", "next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["prettier"],
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double", { avoidEscape: true }],
+      eqeqeq: ["error", "always"],
+      curly: ["error", "all"],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "error",
+      "no-var": "error",
+      "prefer-const": ["error", { destructuring: "all" }],
+      "no-unused-vars": [
+        "warn",
+        { args: "after-used", argsIgnorePattern: "^_" },
+      ],
+      "no-param-reassign": ["error"],
+      "no-shadow": ["error"],
+      complexity: ["warn", { max: 10 }],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "sort-imports": [
+        "warn",
+        {
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-curly-brace-presence": [
+        "error",
+        { props: "never", children: "never" },
+      ],
+      "react/jsx-sort-props": [
+        "warn",
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          ignoreCase: true,
+        },
+      ],
+      "jsx-a11y/anchor-is-valid": [
+        "error",
+        {
+          components: ["Link"],
+          specialLink: ["hrefLeft", "hrefRight"],
+          aspects: ["noHref", "invalidHref", "preferButton"],
+        },
+      ],
+      "jsx-a11y/alt-text": "error",
+      "jsx-a11y/no-autofocus": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": [
+        "warn",
+        {
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true,
+        },
+      ],
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "prettier/prettier": [
+        "error",
+        { singleQuote: false, semi: true, endOfLine: "auto" },
+      ]
+    },
+  }),
 ];
 
 export default eslintConfig;
